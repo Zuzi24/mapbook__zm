462b15f2c2b709d0525bd3f431353be594b19f0b
Zuzi2810 <zuzannamierkiewicz4@gmail.com>
2025-05-12 18:02:48 +0200

    wip: unteractive map

--- utils/controller.py
+++ utils/controller.py
@@ -10,6 +10,7 @@ def add_user(users_data:list)->None:
         new_posts:str=input('podaj liczbę postów: ')
         users_data.append({'name':new_name ,'location': new_location, 'posts': new_posts},)
 
+
 def remove_user(users_data:list)->None:
 
     user_name: str =input('podaj imię znajomego do usunięcia: ')
@@ -25,3 +26,30 @@ def update_user(users_data: list) -> None:
             user['name']=input('podaj nowe imię użytkownika: ')
             user['location']=input('podaj nową lokalizację użytkownika: ')
             user['posts']=input('podaj nową liczbe postów: ')
+
+
+
+
+def get_coordinates(city_name: str) -> list:
+    import requests
+    from bs4 import BeautifulSoup
+    url = f"https://pl.wikipedia.org/wiki/{city_name}"
+    response = requests.get(url).text
+    response_html = BeautifulSoup(response, "html.parser")
+    latitude = float(response_html.select(".latitude")[1].text.replace(",", "."))
+    longitude = float(response_html.select(".longitude")[1].text.replace(",", "."))
+    print(latitude, longitude)
+    return [latitude, longitude]
+
+
+def get_map(users_data: list) -> None:
+    import folium
+    mapa = folium.Map(location=[52.21, 21.0], zoom_start=6)
+    for user in users_data:
+        print(user["location"])
+
+        folium.Marker(
+            location=get_coordinates(user["location"]),
+            popup=f"{user["location"]} {user['name']}",
+        ).add_to(mapa)
+    mapa.save('mapa.html')
